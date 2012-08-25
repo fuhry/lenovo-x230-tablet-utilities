8a789425d228ad94eb9bf1389722a920dca00fb5
Chris Lockfort <clockfort@redhat.com>
2012-08-25 01:07:05 -0400

    whoops, that's probably not going to exist

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 mkdir -p ~/.local/bin/
+mkdir -p ~/.local/user/quicklaunch/
 cp normalize rotate ~/.local/bin/
 cp normalize.desktop rotate.desktop ~/.local/user/quicklaunch/
 xdg-open "https://extensions.gnome.org/extension/57/wwwdotorg-quick-launch/"
